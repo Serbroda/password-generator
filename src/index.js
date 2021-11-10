b85fad9e51c948061ee9189b7abd2d812eee9fbc
Danny Rottstegge <danny@rottstegge.net>
2021-11-10 23:34:41 +0100

    Add prettier config

--- src/index.js
+++ src/index.js
@@ -11,55 +11,55 @@ const doReplaceLetters = false;
 const doAddRandomInt = true;
 
 (async () => {
-  try {
-    console.log(await generatePassword());
-  } catch (e) {
-    console.error(e);
-  }
+    try {
+        console.log(await generatePassword());
+    } catch (e) {
+        console.error(e);
+    }
 })();
 
 async function generatePassword() {
-  let password = "";
+    let password = "";
 
-  for (let i = 1; i <= wordCount; i++) {
-    let word = await getRandomWord();
-    if (doReplaceLetters) {
-      word = replaceLetters(word);
+    for (let i = 1; i <= wordCount; i++) {
+        let word = await getRandomWord();
+        if (doReplaceLetters) {
+            word = replaceLetters(word);
+        }
+        password = password + word;
+        if (i < wordCount) {
+            password = password + separator;
+        }
     }
-    password = password + word;
-    if (i < wordCount) {
-      password = password + separator;
-    }
-  }
 
-  if (doAddRandomInt) {
-    password = password + "-" + getRandomInt(1, 9);
-  }
-  return password;
+    if (doAddRandomInt) {
+        password = password + "-" + getRandomInt(1, 9);
+    }
+    return password;
 }
 
 function replaceLetters(input) {
-  let word = input.replace(/[i|I]/i, "1");
-  word = word.replace(/[a]/i, "@");
-  word = word.replace(/[o|O]/i, "0");
-  word = word.replace(/[s|S]/i, "$");
-  return word;
+    let word = input.replace(/[i|I]/i, "1");
+    word = word.replace(/[a]/i, "@");
+    word = word.replace(/[o|O]/i, "0");
+    word = word.replace(/[s|S]/i, "$");
+    return word;
 }
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 async function getRandomWord() {
-  const html = await request({
-    method: "GET",
-    uri: url,
-    headers: {
-      "user-agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
-    },
-  });
-  return processHtml(html);
+    const html = await request({
+        method: "GET",
+        uri: url,
+        headers: {
+            "user-agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+        },
+    });
+    return processHtml(html);
 }
